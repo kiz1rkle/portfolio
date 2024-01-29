d11e3c265d3c5410736657ce56c488551b100989
Kork <hail3yp@gmail.com>
2024-01-29 11:38:21 -0500

    added more resume page text

--- src/components/pages/ResumePage.jsx
+++ src/components/pages/ResumePage.jsx
@@ -4,6 +4,16 @@ export default function Resume () {
             <h2>Resume</h2>
             <aside id='resume'>
                 <a className="download" href='../../assets/kurtis-p-resume.pdf' download>Download My Resume</a>
+
+                <h3>Kurtis Pullman</h3>
+                <p class='resumep'>
+                Front end web developer leveraging background in art and design to provide unique perspectives on how end-users interact with websites and software platforms. Earned a certificate in Full Stack Web Development from the University Coding Boot Camp. Innovative problem-solver who is passionate about developing apps with a focus on mobile-first design and development. Strengths in creativity, teamwork, and building projects from ideation to execution. 
+                </p>
+
+                <h3>Technical Skills:</h3>
+                <p>
+                JavaScript ES6+, CSS3, HTML5, SQL, NoSQL, GitHub, MongoDB, MySQL, Express, React, Node, Handlebars, jQuery, Bootstrap
+                </p>
             </aside>
         </div>
     )
