08fca16bf1c9eee752f91e35e0239882990ec412
Kork <hail3yp@gmail.com>
2024-02-12 10:30:21 -0500

    adding one quality of life comment

--- src/components/pages/ContactPage.jsx
+++ src/components/pages/ContactPage.jsx
@@ -4,6 +4,7 @@ export default function Contact () {
     return(
         <div id='contact-div'>
             <h2>Contact Me</h2>
+            {/* My instructor told me that form field validation was not a requirement, so this will be for future development */}
             <aside id='contactme'>
                 <h3>Title</h3>
                 <input>
